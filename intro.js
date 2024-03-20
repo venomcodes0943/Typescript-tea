3978fabe354696463cabc5dcb6691899e539cfaf
venomcodes0943 <dhcodes0943@gmail.com>
2024-03-20 07:22:39 +0500

    Intro To TypeScript

---
+++ intro.js
@@ -0,0 +1,6 @@
+var intro = function (name, lang, email) {
+    // Do something Here
+    return "Hello Im ".concat(name, " Learing ").concat(lang, " Mail Me On ").concat(email);
+};
+var firstMeet = intro("Haseeb", "typeScript", "dhcodes0943@gmail.com");
+console.log(firstMeet);
---
+++ intro.ts
@@ -0,0 +1,10 @@
+const intro = (name: string, lang: string, email: string): string => {
+    // Do something Here
+    return `Hello Im ${name} Learing ${lang} Mail Me On ${email}`;
+};
+
+let firstMeet = intro("Haseeb", "typeScript", "dhcodes0943@gmail.com");
+
+console.log(firstMeet);
+
+export { }
